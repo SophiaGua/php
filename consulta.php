b6dfd51673c25abac1caf2fd0cff788c077ca562
sophia <anavitoriabrandaomarques@gmail.com>
2024-08-01 17:47:17 -0300

    ordernar por email

--- consulta.php
+++ consulta.php
@@ -13,8 +13,8 @@
         echo "nenhum registro encontrado.";
     }
     //consulta especifica
-    echo"<BR>Nomes que começam com 'c'<br>";
-    $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'c%'";
+    echo"<BR>Nomes que começam com 's'<br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario where nome like 's%'";
     $resultado = mysqli_query($conexao, $sql);
     echo "<br>";
     if (mysqli_num_rows($resultado) > 0){
@@ -25,5 +25,17 @@
     } else {
         echo "Nenhum registro encontrado!";
     }
-    
+
+    echo"<BR>ordenar os registros por email<br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario order by email";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while ($registro = mysqli_fetch_assoc($resultado)){
+            echo "id: " . $registro["id"]. " nome: " . $registro["nome"].
+            " senha: ". $registro["senha"]. "email:". $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado!";
+    }
 ?>
